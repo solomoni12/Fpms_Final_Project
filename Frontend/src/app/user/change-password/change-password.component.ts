4d98de4bd55253ada38475d9174ef66962dd3895
solomoni12 <mwalupani1234@gmail.com>
2023-05-23 02:39:20 +0300

    updated on 23-05-2023

--- Frontend/src/app/user/change-password/change-password.component.ts
+++ Frontend/src/app/user/change-password/change-password.component.ts
@@ -16,30 +16,36 @@ export class ChangePasswordComponent implements OnInit {
     private service: AuthService,
     private router: Router) { }
 
-    registerform=this.formBuilder.group({
-      password:this.formBuilder.control('', Validators.required),
-      password_confirmation:this.formBuilder.control('', Validators.required),
-      email:this.formBuilder.control('', Validators.compose([Validators.required,Validators.email]))
-    })
+  registerform = this.formBuilder.group({
+    password: this.formBuilder.control('', Validators.required),
+    password_confirmation: this.formBuilder.control('', Validators.required)
+  });
 
-    proceedregistration(){
-      console.log(this.registerform.value);
-      if(this.registerform.valid){
+  proceedregistration() {
+    console.log(this.registerform.value);
+    if (this.registerform.valid) {
+      const password = this.registerform.get('password')?.value;
+      const confirmPassword = this.registerform.get('password_confirmation')?.value;
+
+      if (password === confirmPassword) {
         this.service.changepassword(this.registerform.value)
-          .subscribe(res=>{
-            console.log(res);
-            alertifyjs.success('Password changed sucessful');
-            this.registerform.reset();
-          },
-          error =>{
-            alertifyjs.error('Failed. Please Try Again');
-          }
+          .subscribe(
+            res => {
+              console.log(res);
+              alertifyjs.success('Password changed successfully');
+              this.registerform.reset();
+              this.router.navigate(['/']); // Navigate to home page
+            },
+            error => {
+              alertifyjs.error('Failed. Please try again');
+            }
           );
-          
+      } else {
+        alertifyjs.error('Password and Confirm Password do not match');
       }
-      else{
-        
-      }
+    } else {
+      // Handle form validation errors if needed
+    }
   }
 
   ngOnInit(): void {
